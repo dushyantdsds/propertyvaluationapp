290978b93a77a318f384e4ceb45b34e9df89c7af
Narayan <nspatilme@outlook.com>
2024-08-17 21:35:33 +0530

    Added roles

--- propertyvaluation/propval/models.py
+++ propertyvaluation/propval/models.py
@@ -1,6 +1,17 @@
 from django.db import models
-#from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from django.contrib.auth.models import User
 
-#class CustomUser(AbstractUser):
-  #  userType = models.IntegerField(unique=True)
+class UserDetails(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_email = models.EmailField(unique=True, null=True, blank=True)
+    first_name = models.CharField(max_length=30, null=True, blank=True)
+    last_name = models.CharField(max_length=30, null=True, blank=True)  # Allow null and blank values
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    role = models.CharField(max_length=30, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.email
+
+    def save(self, *args, **kwargs):
+        self.user_email = self.user.email
+        super().save(*args, **kwargs)
--- propertyvaluation/propval/views.py
+++ propertyvaluation/propval/views.py
@@ -2,43 +2,60 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
-# Create your views here.
-#from django.views.decorators.csrf import ensure_csrf_cookie
+from .models import UserDetails
 
-#@ensure_csrf_cookie
 @login_required(login_url='login')
 def Home(request):
-    return render (request,'home.html')
+    user_details = UserDetails.objects.filter(user_email=request.user.email).first()
+    if not user_details:
+        return HttpResponse('User data does not exist')
+    
+    context = {
+        'first_name': user_details.first_name or 'Not present',
+        'last_name': user_details.last_name or 'Not present',
+        'email': request.user.email,
+        'phone': user_details.phone or 'Not present',
+        'role': user_details.role or 'Not present'
+    }
+    
+    if user_details.role == 'admin':
+        return render(request, 'admin_home.html', context)
+    elif user_details.role == 'site_engineer':
+        return render(request, 'site_engineer_home.html', context)
+    elif user_details.role == 'field_engineer':
+        return render(request, 'field_engineer_home.html', context)
+    else:
+        return HttpResponse('Role not present')
 
 def Signup(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         uemail = request.POST.get('email')
         upassword = request.POST.get('password')
         uconfpassword = request.POST.get('confpassword')
-        if upassword!=uconfpassword:
-           return HttpResponse('Password and confirm password does not match')
-        #else:
-        my_user = User.objects.create_user(uemail,uemail,upassword)
+        if upassword != uconfpassword:
+            return HttpResponse('Password and confirm password do not match')
+        
+        if User.objects.filter(email=uemail).exists():
+            return HttpResponse('Email already exists')
+        
+        my_user = User.objects.create_user(uemail, uemail, upassword)
         my_user.save()
-       # return HttpResponse('User has been created')
+        UserDetails.objects.create(user=my_user)
         return redirect('login')
-        print(uemail,upassword,uconfpassword)
-    return render (request,'signup.html')
+    return render(request, 'signup.html')
 
 def Signin(request):
-
-     if request.method=='POST':
+    if request.method == 'POST':
         uemail = request.POST.get('email')
         upassword = request.POST.get('password')
-        user = authenticate(request, username=uemail,password=upassword)
+        user = authenticate(request, username=uemail, password=upassword)
         
         if user is not None:
-           login(request , user)
-           return redirect('home')
+            login(request, user)
+            return redirect('home')
         else:
-            return HttpResponse('user name or password is not correct')   
-        
-     return render (request,'signin.html')
+            return HttpResponse('Username or password is not correct')
+    return render(request, 'signin.html')
 
 def Logout(request):
     logout(request)
